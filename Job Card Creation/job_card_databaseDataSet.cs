1e14bcb1d855f2fd0f575c0acfe547242b5cf259
Aakash Javia <aakashjavia@gmail.com>
2017-07-27 22:32:48 +0530

    First Commit

---
+++ Job Card Creation/job_card_databaseDataSet.cs
@@ -0,0 +1,17 @@
+﻿namespace Job_Card_Creation
+{
+
+
+    partial class job_card_databaseDataSet
+    {
+    }
+}
+
+namespace Job_Card_Creation.job_card_databaseDataSetTableAdapters
+{
+
+
+    public partial class job_cardTableAdapter
+    {
+    }
+}
